81ff5fe915e2afae15614820afc16d901deca889
Lei Wang (Vic) <vic.leiwang@gmail.com>
2020-08-31 21:41:44 +0100

    Disable Kafka rest controller by default to reduce warning logging when Kafka isn't running

--- src/main/java/com/pms/web/rest/PositionKafkaResource.java
+++ src/main/java/com/pms/web/rest/PositionKafkaResource.java
@@ -9,6 +9,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -21,6 +22,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @RestController
+@ConditionalOnProperty("kafka.rest.controller.enabled")
 @RequestMapping("/api/position-kafka")
 public class PositionKafkaResource {
 
